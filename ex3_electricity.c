2a41846386fbbcb94884f6f709df05451b06bed2
zalwangochristine <zalwangochristine11@gmail.com>
2026-03-24 20:41:11 +0300

    Update ex3_electricity.c

--- ex3_electricity.c
+++ ex3_electricity.c
@@ -1,11 +1,17 @@
-/* Name: Your Full Name */
-/* Student Number: Your Registration Number */
-
+/* Name: ZALWANGO CHRISTINE*/
+/* Student Number: 25/U/BIE/01426/PE*/
 #include <stdio.h>
+int main() {
+    float unitsConsumed, costPerUnit, totalBill;
 
-int main(void)
-{
-    /* Write your code here */
+    printf("Enter the number of units consumed: \n");
+    scanf("%f", &unitsConsumed);
+
+    printf("Enter the cost per unit: \n");
+    scanf("%f", &costPerUnit);
+
+    totalBill = unitsConsumed * costPerUnit;
+    printf("Total bill: %.2f\n", totalBill);
 
     return 0;
 }
